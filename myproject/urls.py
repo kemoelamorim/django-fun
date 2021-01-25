f590e19f1c2bdd600e1d4b08ed1d09660057f330
kemoelamorim <kemoel.amorim@gmail.com>
2021-01-25 17:18:09 -0300

    tutorial

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pokemaoc/', include('pokemaoc.urls'))
+    path('pokemaoc/', include('pokemaoc.urls')),
+    path("", include("users.urls"))
 ]
